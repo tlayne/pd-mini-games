cf7f105792f08d87c4fc0a20c33c3e6e51ed678f
Tim <tlayne@users.noreply.github.com>
2023-03-24 13:29:51 -0500

    added animation library
    added AnimateSprite and created implemented idle animation for player

--- source/player.lua
+++ source/player.lua
@@ -3,23 +3,27 @@ import "projectile"
 local pd <const> = playdate
 local gfx <const> = pd.graphics
  
-class('Player').extends(gfx.sprite)
+class('Player').extends(AnimatedSprite)
  
 -- Instantiates the Player
 function Player:init(x, y)
-   local playerImage = gfx.image.new("images/playerImage")
-   assert( playerImage )
-   self:setImage(playerImage)
-   self:moveTo( x, y )
-   self:setCollideRect(2, 4, 20, 32)
-   self:add()
+    local playerTable = gfx.imagetable.new("images/player-table-32-32")
+    -- Set image table to the sprite
+    Player.super.init(self, playerTable)
+
+    self:addState("idle", 1, 3, {tickStep = 2})
+    self:playAnimation()
+    self:moveTo( x, y )
+    self:setCollideRect(5, 2, 22, 30)
+    self:add()
  
-   self.speed = 4
+    self.speed = 4
 end
 
  
 -- Runs every time the playdate refreshes, constantly checking if a button is being presssed (multiple can be pressed at once)
 function Player:update()
+    self:updateAnimation()
    -- Allow player movement with the D-pad
    if playdate.buttonIsPressed( playdate.kButtonUp ) then
        if (self.y > 160) then
@@ -44,7 +48,7 @@ function Player:update()
    if pd.buttonJustPressed(pd.kButtonA) then
         local projectileActive = projectileLimit()
         if (projectileActive < 4) then
-            Projectile(self.x, self.y - 15, 5)
+            Projectile(self.x, self.y - 18, 5)
         end
    end
 end
